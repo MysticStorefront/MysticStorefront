e9a8385984997f16e9df677712a97778ea9d5a7c
Aaryan Bajaj <aaryan.b076@gmail.com>
2023-07-24 13:43:38 +0530

    Making the image cover the entire screen

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,28 @@
 import React from "react";
 import "../App.css";
 import ImageCard from "./ImageCard";
+import LandingPage from "./LandingPage";
 
 export default function Home() {
   return (
     <>
-      <div className="section-hero-image"></div>
+      <LandingPage />
       <div className="section-introduction">
         <span className="header-2">
           <h1 style={{ fontSize: 50 }}>Norwood Interiors</h1>
         </span>
         <span className="paragraph-2">
-            <p>LLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+          <p>
+            LLorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum
+          </p>
         </span>
       </div>
       <div className="portfolio">
